6a29bf7cfc68d18d91834b3210792f1e67cc4b9f
Soma Beleznay <belezn1@stolaf.edu>
2018-01-23 19:26:59 -0600

    Librarian.searchPotentialRecipes() now takes Ingredients object with names in it as an argument, and returns a list of recipes

--- Ingredients.java
+++ Ingredients.java
@@ -7,5 +7,7 @@ public class Ingredients {
     public int iid;
 
     public List<String> ingredientnames = new ArrayList<String>();
+
+    public List<Integer> ingredientIDs = new ArrayList<Integer>();
     //public String iname;
 }
